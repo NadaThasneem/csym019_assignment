41e785e04ce1d8c176f5d22a22d34eb9930671d8
Nada Thasneem <nadaminnu555@gmail.com>
2022-05-07 16:16:28 +0100

    fifth commit

--- task1/recipe.js
+++ task1/recipe.js
@@ -14,8 +14,44 @@ function readTextFile(){
             var ingredientsData = data[i].ingredients;
     
             var ingredients="";
+    
+            for(j=0;j<ingredientsData.length;j++){
+                ingredients = ingredients + "<br>" + ingredientsData[j];
+            }
+    
+            var methodData = data[i].method;
+            var methods="";
+    
+            for(j=0;j<methodData.length;j++){
+                methods=methods+ "<br>" +methodData[j];
+            }
+            var preptimeData = data[i].preptime;
+            var preptimes=0;
+    
+            for(j=0;j<preptimeData.length;j++){
+                preptimes=preptimes+preptimeData[j];
+            }
+    
+            var cooktimeData = data[i].cooktime;
+            var cooktimes=0;
+    
+            for(j=0;j<cooktimeData.length;j++){
+                cooktimes=cooktimes+cooktimeData[j];
+            }
+    
+            var nutritionData = data[i].nutrition;
+            var nutritions="";
+    
+            for(j=0;j<nutritionData.length;j++){
+                nutritions=nutritions+ "<br>" +nutritionData[j];
+            }
+    
+            str = str+"<tr><td>"+(i+1)+"</td>"+"<td>"+"<img src="+image+" width="+100+"vw height="+100+"vh></td>"+"<td>"+name+"</td>"+"<td>"+ingredients+"</td>"+"<td>"+methods+"</td>"+"<td>"+preptimes+" min</td>"+"<td>"+cooktimes+" min</td>"+"<td>"+nutritions+"</td></tr>";
         }
+        document.getElementById("data").innerHTML = str;
     }
-}
-    )
-}
+    });
+  }
+  $(document).ready(function() {
+    setInterval(readTextFile, 5000);
+});
