ff6fdd7d3d49f004d3e71cb87646cda3d8625de4
Nada Thasneem <nadaminnu555@gmail.com>
2022-05-10 00:23:50 +0530

    eighth commit

--- task1/recipe.js
+++ task1/recipe.js
@@ -1,15 +1,3 @@
-// function readTextFile(file, callback) {
-//     var rawFile = new XMLHttpRequest();
-//     rawFile.overrideMimeType("application/json");
-//     rawFile.open("GET", file, true);
-//     rawFile.onreadystatechange = function() {
-//         if (rawFile.readyState === 4 && rawFile.status == "200") {
-
-//             callback(rawFile.responseText);
-//         }
-//     }
-//     rawFile.send(null);
-// }
 
 function readTextFile(){
     $.ajax({ 
@@ -22,8 +10,9 @@ function readTextFile(){
         str="";
         for(i = 0; i<data.length; i++){
             var image = data[i].image;
-            var name = "<p style=\"font-size:1.2rem\"> <strong>"+data[i].name + "</strong> </p>";
-    
+            var name = "<p style=\"font-size:1.4rem\"> <strong>"+data[i].name + "</strong> </p>";
+            var author = data[i].author;
+
             var ingredientsData = data[i].ingredients;
     
             var ingredients="<ul class=\"list-group list-group-flush\">";
@@ -43,19 +32,9 @@ function readTextFile(){
 
             methods=methods+"</ul>"
 
-            var preptimeData = data[i].preptime;
-            var preptimes=0;
+            var preptimes=data[i].preptime;
     
-            for(j=0;j<preptimeData.length;j++){
-                preptimes=preptimes+preptimeData[j];
-            }
-    
-            var cooktimeData = data[i].cooktime;
-            var cooktimes=0;
-    
-            for(j=0;j<cooktimeData.length;j++){
-                cooktimes=cooktimes+cooktimeData[j];
-            }
+            var cooktimes=data[i].cooktime;
     
             var nutritionData = data[i].nutrition;
             var nutritions="<ul class=\"list-group list-group-flush\">";
@@ -66,61 +45,14 @@ function readTextFile(){
 
             nutritions = nutritions + "</ul>"
     
-            str = str+"<tr><td scope="+"row"+">"+(i+1)+"</td>"+"<td class=\"align-middle\">"+"<img src="+image+" width="+100+"vw height="+100+"vh></td>"+"<td>"+name+"</td>"+"<td>"+ingredients+"</td>"+"<td>"+methods+"</td>"+"<td>"+preptimes+" min</td>"+"<td>"+cooktimes+" min</td>"+"<td>"+nutritions+"</td></tr>";
+            str = str+"<tr><td scope="+"row"+">"+(i+1)+"</td>"+"<td class=\"align-middle\">"+"<img src="+image+" width="+100+"vw height="+100+"vh></td>"+"<td>"+name+"</td>"+"<td>"+author+"</td>"+"<td>"+ingredients+"</td>"+"<td>"+methods+"</td>"+"<td>"+preptimes+" min</td>"+"<td>"+cooktimes+" min</td>"+"<td>"+nutritions+"</td></tr>";
         }
         document.getElementById("data").innerHTML = str;
     }
     });
   }
 
-//usage:
-// readTextFile("recipe.json", function(text) {
-//     var data = JSON.parse(text);
-//     console.log(data);
-//     str="";
-//     for(i = 0; i<data.length; i++){
-//         var image = data[i].image;
-//         var name = data[i].name;
 
-//         var ingredientsData = data[i].ingredients;
-
-//         var ingredients="";
-
-//         for(j=0;j<ingredientsData.length;j++){
-//             ingredients = ingredients + "<br>" + ingredientsData[j];
-//         }
-
-//         var methodData = data[i].method;
-//         var methods="";
-
-//         for(j=0;j<methodData.length;j++){
-//             methods=methods+ "<br>" +methodData[j];
-//         }
-//         var preptimeData = data[i].preptime;
-//         var preptimes=0;
-
-//         for(j=0;j<preptimeData.length;j++){
-//             preptimes=preptimes+preptimeData[j];
-//         }
-
-//         var cooktimeData = data[i].cooktime;
-//         var cooktimes=0;
-
-//         for(j=0;j<cooktimeData.length;j++){
-//             cooktimes=cooktimes+cooktimeData[j];
-//         }
-
-//         var nutritionData = data[i].nutrition;
-//         var nutritions="";
-
-//         for(j=0;j<nutritionData.length;j++){
-//             nutritions=nutritions+ "<br>" +nutritionData[j];
-//         }
-
-//         str = str+"<tr><td>"+(i+1)+"</td>"+"<td>"+"<img src="+image+" width="+100+"vw height="+100+"vh></td>"+"<td>"+name+"</td>"+"<td>"+ingredients+"</td>"+"<td>"+methods+"</td>"+"<td>"+preptimes+" min</td>"+"<td>"+cooktimes+" min</td>"+"<td>"+nutritions+"</td></tr>";
-//     }
-//     document.getElementById("data").innerHTML = str;
-// });
 
 $(document).ready(function() {
     setInterval(readTextFile, 5000);
